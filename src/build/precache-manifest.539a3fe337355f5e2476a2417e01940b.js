2640e84d6815cc977b40208da32853b3edadf826
DH-CyberCode <61819588+DH-CyberCode@users.noreply.github.com>
2020-09-16 21:15:10 -0400

    First Commit

---
+++ src/build/precache-manifest.539a3fe337355f5e2476a2417e01940b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "559fe4787faba3f37824f7aafb4c1c5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cf632d56b9aa382703a",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "17f1f2c1e2cd4f440342",
+    "url": "/static/js/2.3d761804.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3d761804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cf632d56b9aa382703a",
+    "url": "/static/js/main.d278e19a.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  },
+  {
+    "revision": "4a407629c91e02f1b056b2879ca7d76f",
+    "url": "/static/media/fail-whale.4a407629.jpg"
+  }
+]);
---
+++ src/server.js
@@ -0,0 +1,80 @@
+import express from 'express';
+import bodyParser from 'body-parser'
+import {MongoClient} from 'mongodb';
+import path from 'path';
+
+
+
+//init express server
+const app = express();
+
+//add module for parsing json body of request to server
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '/build')));
+
+const withDB = async (operations, res) => {
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true });
+        const db = client.db('my-blog');
+    
+        await operations(db);
+    
+        client.close();
+    } catch (error) {
+        res.status(500).json({ message: 'Error connecting to db', error });
+    }
+}
+
+// Handle a Get req retrieve articles from MongoDB
+app.get('/api/articles/:name', async (req, res) => {
+    withDB(async (db) => {
+        const articleName = req.params.name;
+
+        const articleInfo = await db.collection('articles').findOne({ name: articleName })
+        res.status(200).json(articleInfo);
+    }, res);
+})
+
+// Handle a POST req to increment upvotes on articles
+app.post('/api/articles/:name/upvote', async (req, res) => {
+    withDB(async (db) => {
+        const articleName = req.params.name;
+    
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        await db.collection('articles').updateOne({ name: articleName }, {
+            '$set': {
+                upvotes: articleInfo.upvotes + 1,
+            },
+        });
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+    
+        res.status(200).json(updatedArticleInfo);
+    }, res);
+});
+
+//Handle POST req to server for adding comments to articles
+app.post('/api/articles/:name/add-comment', (req, res) => {
+    const { username, text } = req.body;
+    const articleName = req.params.name;
+
+    withDB(async (db) => {
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        await db.collection('articles').updateOne({ name: articleName }, {
+            '$set': {
+                comments: articleInfo.comments.concat({ username, text }),
+            },
+        });
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+
+        res.status(200).json(updatedArticleInfo);
+    }, res);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
+//starts the Express server
+app.listen(8000, () => console.log('Listening on port 8000'))
+
